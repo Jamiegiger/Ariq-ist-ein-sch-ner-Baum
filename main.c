cf8f878d37d087012f58aa81306aba08c6d183f5
Jamiegiger <jamie.giger@noseryoung.com>
2022-10-05 15:35:36 +0200

    ariq ist ein schoener baum

---
+++ main.c
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jamie
+ *
+ * Created on 5. Oktober 2022, 14:28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc,      char** argv) { //Ariq ist ein schöner Baum mit einem schönen Busch und Stamm :)
+    int number; //chill mal
+    printf ("u can choose and tipe any number u want and see if its even or odd\n");  scanf ("%d", &number); 
+    if 
+            (number %2 
+            == 0) {
+    printf ("the number u typed is even\n");} //Dönermann.xxl
+    
+    else                  {printf("the number is odd");}
+    
+    
+    return 69;
+                  
+
+}
+
